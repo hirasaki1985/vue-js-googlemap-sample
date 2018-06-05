320682cf3ffed27ff1cf2a4f26b22a37d902a244
hirasaki1985 <hirasaki1985@gmail.com>
2018-06-05 17:05:08 +0900

    マーカーとクリックした時のコンテンツ表示機能を追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,7 @@ export default {
       msg: 'Welcome to Your Vue.js App'
     }
   },
+
   mounted () {
     const map = new GMaps({
       el: '#map',
@@ -24,16 +25,92 @@ export default {
     })
     console.log(map)
     Vue.prototype.$map = map
+
+    this.$map.addMarker({
+      lat: -12.043330,
+      lng: -77.028330,
+      title: 'Lima',
+      click: (e) => {
+        console.log('click')
+        if (this.$map.overlays.length === 0) {
+          this.drawLabelInfo(this)
+        } else {
+          this.$map.removeOverlays()
+        }
+      }
+    })
+  },
+
+  methods: {
+    drawLabelInfo (el) {
+      // let self = this
+
+      this.$map.drawOverlay({
+        lat: -12.043330,
+        lng: -77.028330,
+        layer: 'floatPane',
+        verticalOffset: -170,
+        content: '<div class="map-overlay">' +
+        `<button id="map-overlay-close">close</button>` +
+        `<div class="contents clearfix">コンテンツ</div>` +
+        '</div></div>' +
+        '</div>'
+      })
+
+      let overlayinterval = setInterval(() => {
+        let overlayel = document.getElementById('map-overlay-close')
+        console.log('setInterval()')
+        console.log(overlayel)
+        console.log(typeof (overlayel))
+        if (typeof (overlayel) === 'object') {
+          console.log('add event')
+          overlayel.addEventListener('click', closeOverlay(), false)
+          clearInterval(overlayinterval)
+        }
+        // clearInterval(overlayinterval)
+      }, 1000)
+
+      let closeOverlay = function () {
+        return function () {
+          el.$map.removeOverlays()
+        }
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss">
 #map {
   width: 600px;
   height: 600px;
   margin:auto;
   padding:auto;
+}
+
+.map-overlay {
+  border: 1px solid gray;
+  background-color: white;
+  border-radius: 10px;
+  padding: 5px;
+  width: 200px;
+}
+
+.field {
+  padding: 5px 15px;
+  border-bottom: 1px solid gray;
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  .label {
+    color: darkgray;
+  }
+
+  .battery {
+    float: right;
+  }
 }
 </style>
